e41a9bd58f4368194de885005d388fd14b6a6165
Souvik <souvikmitra1234@gmail.com>
2025-09-10 15:05:09 +0530

    test 1 cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,24 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || []
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true) // allow server-to-server or curl
-
-      // allow if explicitly listed
-      if (allowedOrigins.includes(origin)) return callback(null, true)
-
-      // allow all Vercel preview deployments (*.vercel.app)
-      if (/\.vercel\.app$/.test(origin)) return callback(null, true)
-
-      // otherwise block
-      callback(new Error(`CORS blocked for origin: ${origin}`))
-    },
-    credentials: true,
-  })
-)
-
+app.use(cors({ origin: "*" })) // temporarily allow all for testing
 app.use("/", appRoutes)
 ;(async () => {
   await connectDB()
